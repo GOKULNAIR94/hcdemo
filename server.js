3037097d74ffcd1f4a37a10a6429139e5bc76fb8
Gokul Nair <gokulgnair94@gmail.com>
2019-01-09 19:12:41 +0530

    ui done

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ restService.use(bodyParser.json());
 
 restService.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
 });
 
