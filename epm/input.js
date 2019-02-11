fc6a970f81fed1e08787724763127134a7b038c7
GOKUL GOPALAKRISHNAN Nair <GOKULGOPALAKRN@hcl.com>
2019-02-11 19:15:19 +0530

    epm

--- epm/input.js
+++ epm/input.js
@@ -2,11 +2,14 @@ module.exports = function( dummy, anaConfig, req, res, callback) {
     
     var input = {};
     var intentName = req.body.result.metadata.intentName;
+    var appName = req.body.result.parameters.epm_application;
+    if( appName == "" || appName == null )
+        appName = "vision";
     
     switch (true) {
         case (intentName == "EPM_MDXQuery"):
             {
-                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/ITOH/dataexport/plantypes/Plan1";
+                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/" + appName + "/dataexport/plantypes/Plan1";
                 
                 input.body = "mdxQuery=SELECT {[Period].[" + req.body.result.parameters.Period + "]} ON COLUMNS, {[Account].[" + req.body.result.parameters.epm_account + "]} ON ROWS FROM Vision.Plan1 WHERE ([Year].[" + req.body.result.parameters.epm_year + "],[Scenario].[" + req.body.result.parameters.epm_scenario + "],[Version].[" + req.body.result.parameters.epm_version + "],[Entity].[" + "403" + "],[Product].[" + "No Product" + "])";
                 callback(input);
@@ -15,7 +18,7 @@ module.exports = function( dummy, anaConfig, req, res, callback) {
             
         case (intentName == "EPM_Jobs"):
             {
-                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/ITOH/jobs";
+                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/" + appName + "/jobs";
                 input.body = {
                     "jobType" : "CUBE_REFRESH",
                     "jobName": "CubeRefresh"
@@ -44,7 +47,7 @@ module.exports = function( dummy, anaConfig, req, res, callback) {
                     }
                 
                 console.log("jobid : " + jobId);
-                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/ITOH/jobs/" + jobId;
+                input.qString = "/HyperionPlanning/rest/11.1.2.4/applications/" + appName + "/jobs/" + jobId;
                 callback(input);
                 break;
             }
